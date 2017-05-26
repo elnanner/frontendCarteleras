fdfc30fdd31fa32cd2f3c6b7afc727ba71d14c46
Luciano <elnanner2@gmail.com>
2017-05-26 00:23:21 -0300

    create user(falta la persistencia) y listado de users(listo)- angular maldito

--- WebContent/app/controllers/altaController.js
+++ WebContent/app/controllers/altaController.js
@@ -1,5 +1,22 @@
 var app = angular.module("app");//obtenemos la el modulo para poder usarlo
 
-app.controller("altaController", function($scope){
+app.controller("altaController", function($scope, $http, $location){//$http ,$location son servicios que importamos
+	$scope.lista = [{nombre: "luciano"}, {nombre : "ramiro"}, {nombre:"sara"}];
+	
+	//functio que guarda un usuario 
+	$scope.saveUser = function(){
+		alert("saraza");
+		$http({
+			url: "localhost:8080/backendCarteleras/createUser",
+			method: "POST",
+			data: $scope.usuario,
+			headers: {"Content-Type": "application/x-www-form-urlencoded"}
+		}).then(//si todo fue correcto
+			function(respuesta){
+				$location.path('/');
+			}
+		)
+		
+	}
 	
 });
--- WebContent/app/controllers/topMenuOptionController.js
+++ WebContent/app/controllers/topMenuOptionController.js
@@ -1,6 +1,7 @@
+/*
 app.config(function($routeProvider ,$locationProvider) {
 	    $routeProvider
-	    .when('/createUser',{
+	    .when('/createUsers',{
 	    	templateUrl: 'pages/userOperations/createUser.html',
 	    	controller: 'topMenuOptionController'
 	    })
@@ -9,14 +10,12 @@ app.config(function($routeProvider ,$locationProvider) {
 	    	controller: 'topMenuOptionController'
 	    })
 	});
-
-app.controller('topMenuOptionController',['$scope','$htpp',function($scope,$http){
-	
-
-	
-	$scope.createUser= function(){
-		alert(" createUser");
-		var data={"user": $scope.name, "pass": $scope.pass, "mail": $scope.mail, "type": $scope.type};	
+*/
+var app = angular.module("app");//obtenemos la el modulo para poder usarlo
+app.controller("topMenuOptionController", function($scope, $http, $location, $resource){
+	$scope.createUser = function(){
+		alert(" createUser Nuevo");
+		/*var data={"user": $scope.name, "pass": $scope.pass, "mail": $scope.mail, "type": $scope.type};	
 		    alert(JSON.stringify(data));
 	      /*  $http.post("http://localhost:8080/backendCarteleras/createUser/", data)
 	        	.success(function(response, status) {
@@ -30,11 +29,37 @@ app.controller('topMenuOptionController',['$scope','$htpp',function($scope,$http
 	        	 }); 
 		*/
 		 
+	}
+	$scope.saludar = function(){
+		alert("saludando");
+	};
+	
+	$scope.saveUser = function(){
+		alert("usuario ingresado correctamente");
+		
+		$http({
+			url: "http://localhost:8080/backendCarteleras/createUser",
+			method: "POST",
+			data: $scope.usuario,
+			headers: {"Content-Type": "application/x-www-form-urlencoded"}
+		}).then(//si todo fue correcto
+			function(respuesta){
+				$location.path('/');
+			}
+		)
+		
+		
 	};
 	
 	
-	
-	
-	
-	
-}]);
+		$http({
+		    method: 'GET', 
+		    url: 'http://localhost:8080/backendCarteleras/users'
+		  }).success(function(data, status, headers, config) {
+		      $scope.users=data;
+		  }).error(function(data, status, headers, config) {
+		      alert("Ha fallado la petición. Estado HTTP:"+status);
+		  });
+			
+		
+});
