6b808a1bf691d93d2ee40ff176d31d8b1b990445
Luciano <elnanner2@gmail.com>
2017-05-26 23:59:32 -0300

    reacomodado el controlador del topmenu

--- WebContent/app/controllers/topMenuOptionController.js
+++ WebContent/app/controllers/topMenuOptionController.js
@@ -13,46 +13,9 @@ app.config(function($routeProvider ,$locationProvider) {
 */
 var app = angular.module("app");//obtenemos la el modulo para poder usarlo
 app.controller("topMenuOptionController", function($scope, $http, $location, $resource){
-	$scope.createUser = function(){
-		alert(" createUser Nuevo");
-		/*var data={"user": $scope.name, "pass": $scope.pass, "mail": $scope.mail, "type": $scope.type};	
-		    alert(JSON.stringify(data));
-	      /*  $http.post("http://localhost:8080/backendCarteleras/createUser/", data)
-	        	.success(function(response, status) {
-	        		$scope.mensajeError='';
-	        		
-	    			$locationProvider.path('/listUsers');
-	        	 }).error(function(response){
-	        		 
-	        		 $scope.mensajeError='error';
-	        	
-	        	 }); 
-		*/
-		 
-	}
-	$scope.saludar = function(){
-		alert("saludando");
-	};
-	
-	$scope.saveUser = function(){
-		alert("usuario ingresado correctamente"+usuario);
-		
-		$http({
-			url: "http://localhost:8080/backendCarteleras/createUser",
-			method: "POST",
-			data: $scope.usuario.type,
-			headers: {"Content-Type": "application/x-www-form-urlencoded"}
-		}).then(//si todo fue correcto
-			function(respuesta){
-				$location.path('/listUsers');
-			}
-		)
-		
-		
-	};
-	
-	
-		$http({
+		/*
+		 * esto es de listController, lo paso allá
+		 * $http({
 		    method: 'GET', 
 		    url: 'http://localhost:8080/backendCarteleras/users'
 		  }).success(function(data, status, headers, config) {
@@ -61,5 +24,5 @@ app.controller("topMenuOptionController", function($scope, $http, $location, $re
 		      alert("Ha fallado la petición. Estado HTTP:"+status);
 		  });
 			
-		
+		*/
 });
