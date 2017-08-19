1b4bcfdf45f0f94a6070b1044c5544e6a0fbea1f
Luciano Matsuo <matsuoluciano@gmail.com>
2017-08-18 22:53:19 -0300

    modificación del servicio, funciona bien ahora

--- WebContent/app/services/board.service.js
+++ WebContent/app/services/board.service.js
@@ -1,5 +1,18 @@
 'use strict';
 angular.module('app')
 .factory('Board',function($resource){
-	return $resource("http://localhost:8080/backendCarteleras/boards/:id",{id: "@id"});
+	var resource =$resource("http://localhost:8080/backendCarteleras/boards/:id",{id: "@id"});	
+	
+	var listar = function(){
+		return resource.query();
+	}
+	
+	var obtener = function(id){
+		return resource.get({id: id});
+	}
+	
+	return{
+		list: listar,
+		get: obtener
+	}
 })
